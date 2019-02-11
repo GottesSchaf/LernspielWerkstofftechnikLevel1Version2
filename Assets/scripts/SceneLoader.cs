833a0e89594359aa4cb90b9b21371fc2ccd01e08
Hannah <hanndragggon@gmail.com>
2019-02-11 12:35:09 +0100

    Hauptmenü eingefügt

---
+++ Assets/scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void Level1()
+    {
+        SceneManager.LoadScene("level1", LoadSceneMode.Single);
+    }
+
+    public void SpielBeenden()
+    {
+        Application.Quit();
+    }
+}
