fb83580d1974afea2b1a45f0ab1873fc4279af06
Hannah <hanndragggon@gmail.com>
2019-02-13 13:35:10 +0100

    Texteditor angepasst, Bilder ersetzt, etc.

--- Assets/scripts/TextEditor/TextFileReader.cs
+++ Assets/scripts/TextEditor/TextFileReader.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,6 +10,15 @@ using UnityEngine.UI;
 public class TextFileReader : MonoBehaviour
 {
     #region Variables
+
+    public enum Level
+    {
+        Menu,
+        Game
+    }
+
+    [SerializeField]
+    private Level levelMode;
 
     [SerializeField]
     private GameObject[] ingameTexts;
@@ -24,29 +34,93 @@ public class TextFileReader : MonoBehaviour
         get { return bb; }
     }
 
-    public string bbtext;
+    [SerializeField]
+    private GameObject book;
+    public GameObject Book
+    {
+        get { return book; }
+    }
 
+    private List<Book> books;
     #endregion
 
     #region Methods
 
-    // Reads the TextFile on the given Path and puts the String in the right Textbox
+    private void Awake()
+    {
+        if(levelMode == Level.Game)
+        {
+            books = new List<Book>();
+            foreach (Book b in book.GetComponentsInChildren<Book>())
+            {
+                books.Add(b);
+            }
+        }
+    }
+
+    // Reads the TextFile on the given Path and puts the String in the right Textbox or Book URL
     public void ReadTextFile(string path)
     {
-        string rawText = File.ReadAllText(@path, System.Text.Encoding.Default);
-
-        string[] textFileRegions = rawText.Split('|');
-
-        foreach(string s in textFileRegions)
+        if (levelMode == Level.Game)
         {
-            string[] splitText = s.Split('~');
+            string rawText = File.ReadAllText(@path, System.Text.Encoding.Default);
 
-            foreach(GameObject x in ingameTexts)
+            byte[] decbuff = Convert.FromBase64String(rawText);
+            rawText = Encoding.UTF8.GetString(decbuff);
+
+            string[] textFileRegions = rawText.Split('|');
+
+            int i = 0;
+
+            foreach (string s in textFileRegions)
             {
-                if (x.name == splitText[0].Trim())
+                string[] splitText = s.Split('~');
+
+                foreach (GameObject x in ingameTexts)
                 {
-                    x.GetComponent<Text>().text = Regex.Replace(splitText[1], @"\t|\n|\r", "");
-                    break;
+                    if (x.name == splitText[0].Trim())
+                    {
+                        x.GetComponent<Text>().text = Regex.Replace(splitText[1], @"\t|\n|\r", "");
+                        break;
+                    }
+                }
+
+
+                foreach (Book b in books)
+                {
+                    if (!splitText[0].Contains("BuchLink"))
+                    {
+                        break;
+                    }
+                    else if (splitText[0].Contains("BuchLink") && i < books.Count)
+                    {
+                        books[i].URL = Regex.Replace(splitText[1], @"\t|\n|\r", "");
+                        i++;
+                        break;
+                    }
+                }
+            }
+        }
+        else if (levelMode == Level.Menu)
+        {
+            string rawText = File.ReadAllText(@path, System.Text.Encoding.Default);
+
+            byte[] decbuff = Convert.FromBase64String(rawText);
+            rawText = Encoding.UTF8.GetString(decbuff);
+
+            string[] textFileRegions = rawText.Split('|');
+
+            foreach (string s in textFileRegions)
+            {
+                string[] splitText = s.Split('~');
+
+                foreach (GameObject x in ingameTexts)
+                {
+                    if (x.name == splitText[0].Trim())
+                    {
+                        x.GetComponent<Text>().text = Regex.Replace(splitText[1], @"\t|\n|\r", "");
+                        break;
+                    }
                 }
             }
         }
@@ -54,127 +128,137 @@ public class TextFileReader : MonoBehaviour
 
     public void ReadTextFileBB(string path)
     {
-        string rawText = Regex.Replace(File.ReadAllText(@path, System.Text.Encoding.Default), @"\t|\n|\r", "");
-        
-        string[] textFileRegions = rawText.Split('|');
-
-        foreach (string s in textFileRegions)
+        if(levelMode == Level.Game)
         {
-            string[] splitText = s.Split('~');
-            bbtext += "\n" + splitText[1];
-            switch (splitText[0])
+            string rawText = Regex.Replace(File.ReadAllText(@path, System.Text.Encoding.Default), @"\t|\n|\r", "");
+
+            byte[] decbuff = Convert.FromBase64String(rawText);
+            rawText = Encoding.UTF8.GetString(decbuff);
+
+            string[] textFileRegions = rawText.Split('|');
+
+            foreach (string s in textFileRegions)
             {
-                case "4.Wert_Verwendet":
-                    if (Regex.Replace(splitText[1], @"\t|\n|\r", "").ToLower() == "wahr")
-                    {
-                        bb.graphPunkt4 = true;
-                    }
-                    else
-                    {
-                        bb.graphPunkt4 = false;
-                    }
-                    break;
-                case "5.Wert_Verwendet":
-                    if (Regex.Replace(splitText[1], @"\t|\n|\r", "").ToLower() == "wahr")
-                    {
-                        bb.graphPunkt5 = true;
-                    }
-                    else
-                    {
-                        bb.graphPunkt5 = false;
-                    }
-                    break;
-                case "BB1_Zieltemperatur":
-                    int z = 0;
+                string[] splitText = s.Split('~');
+                switch (splitText[0])
+                {
+                    case "4.Wert_Verwendet":
+                        if (Regex.Replace(splitText[1], @"\t|\n|\r", "").ToLower() == "wahr")
+                        {
+                            bb.graphPunkt4 = true;
+                        }
+                        else
+                        {
+                            bb.graphPunkt4 = false;
+                        }
+                        break;
+                    case "5.Wert_Verwendet":
+                        if (Regex.Replace(splitText[1], @"\t|\n|\r", "").ToLower() == "wahr")
+                        {
+                            bb.graphPunkt5 = true;
+                        }
+                        else
+                        {
+                            bb.graphPunkt5 = false;
+                        }
+                        break;
+                    case "BB1_Zieltemperatur":
+                        int z = 0;
 
-                    foreach (string x in splitText[1].Split(new string[] { "Element :" }, StringSplitOptions.None))
-                    {
-                        if (z > 0)
+                        foreach (string x in splitText[1].Split(new string[] { "Element :" }, StringSplitOptions.None))
                         {
-                            bb.BB1_Zieltemp[z - 1] = (float)Convert.ToDouble(Regex.Replace(x, @"\t|\n|\r| ", ""));
+                            if (z > 0)
+                            {
+                                bb.BB1_Zieltemp[z - 1] = (float)Convert.ToDouble(Regex.Replace(x, @"\t|\n|\r| ", ""));
+                            }
+                            z++;
                         }
-                        z++;
-                    }
-                    break;
-                case "BB2_Zieltemperatur":
-                    int zz = 0;
-                    foreach (string x in splitText[1].Split(new string[] { "Element :" }, StringSplitOptions.None))
-                    {
-                        if (zz > 0)
+                        break;
+                    case "BB2_Zieltemperatur":
+                        int zz = 0;
+                        foreach (string x in splitText[1].Split(new string[] { "Element :" }, StringSplitOptions.None))
                         {
-                            bb.BB2_Zieltemp[zz - 1] = (float)Convert.ToDouble(Regex.Replace(x, @"\t|\n|\r| ", ""));
+                            if (zz > 0)
+                            {
+                                bb.BB2_Zieltemp[zz - 1] = (float)Convert.ToDouble(Regex.Replace(x, @"\t|\n|\r| ", ""));
+                            }
+                            zz++;
                         }
-                        zz++;
-                    }
-                    break;
-                case "BB3_Zieltemperatur":
-                    int zzz = 0;
-                    foreach (string x in splitText[1].Split(new string[] { "Element :" }, StringSplitOptions.None))
-                    {
-                        if (zzz > 0)
+                        break;
+                    case "BB3_Zieltemperatur":
+                        int zzz = 0;
+                        foreach (string x in splitText[1].Split(new string[] { "Element :" }, StringSplitOptions.None))
                         {
-                            bb.BB3_Zieltemp[zzz - 1] = (float)Convert.ToDouble(Regex.Replace(x, @"\t|\n|\r| ", ""));
+                            if (zzz > 0)
+                            {
+                                bb.BB3_Zieltemp[zzz - 1] = (float)Convert.ToDouble(Regex.Replace(x, @"\t|\n|\r| ", ""));
+                            }
+                            zzz++;
                         }
-                        zzz++;
-                    }
-                    break;
-                case "BB4_Zieltemperatur":
-                    int zzzz = 0;
-                    foreach (string x in splitText[1].Split(new string[] { "Element :" }, StringSplitOptions.None))
-                    {
-                        if (zzzz > 0)
+                        break;
+                    case "BB4_Zieltemperatur":
+                        int zzzz = 0;
+                        foreach (string x in splitText[1].Split(new string[] { "Element :" }, StringSplitOptions.None))
                         {
-                            bb.BB4_Zieltemp[zzzz - 1] = (float)Convert.ToDouble(Regex.Replace(x, @"\t|\n|\r| ", ""));
+                            if (zzzz > 0)
+                            {
+                                bb.BB4_Zieltemp[zzzz - 1] = (float)Convert.ToDouble(Regex.Replace(x, @"\t|\n|\r| ", ""));
+                            }
+                            zzzz++;
                         }
-                        zzzz++;
-                    }
-                    break;
-                case "BB1_Zeit":
-                    int o = 0;
-                    foreach (string x in splitText[1].Split(new string[] { "Element :" }, StringSplitOptions.None))
-                    {
+                        break;
+                    case "BB1_Zeit":
+                        int o = 0;
+                        foreach (string x in splitText[1].Split(new string[] { "Element :" }, StringSplitOptions.None))
+                        {
 
-                        if (o > 0)
-                        {
-                            bb.BB1_Zeit[o - 1] = Convert.ToInt32(Regex.Replace(x, @"\t|\n|\r| ", ""));
+                            if (o > 0)
+                            {
+                                bb.BB1_Zeit[o - 1] = Convert.ToInt32(Regex.Replace(x, @"\t|\n|\r| ", ""));
+                            }
+                            o++;
                         }
-                        o++;
-                    }
-                    break;
-                case "BB2_Zeit":
-                    int oo = 0;
-                    foreach (string x in splitText[1].Split(new string[] { "Element :" }, StringSplitOptions.None))
-                    {
-                        if (oo > 0)
+                        break;
+                    case "BB2_Zeit":
+                        int oo = 0;
+                        foreach (string x in splitText[1].Split(new string[] { "Element :" }, StringSplitOptions.None))
                         {
-                            bb.BB2_Zeit[oo - 1] = Convert.ToInt32(Regex.Replace(x, @"\t|\n|\r| ", ""));
+                            if (oo > 0)
+                            {
+                                bb.BB2_Zeit[oo - 1] = Convert.ToInt32(Regex.Replace(x, @"\t|\n|\r| ", ""));
+                            }
+                            oo++;
                         }
-                        oo++;
-                    }
-                    break;
-                case "BB3_Zeit":
-                    int ooo = 0;
-                    foreach (string x in splitText[1].Split(new string[] { "Element :" }, StringSplitOptions.None))
-                    {
-                        if (ooo > 0)
+                        break;
+                    case "BB3_Zeit":
+                        int ooo = 0;
+                        foreach (string x in splitText[1].Split(new string[] { "Element :" }, StringSplitOptions.None))
                         {
-                            bb.BB3_Zeit[ooo - 1] = Convert.ToInt32(Regex.Replace(x, @"\t|\n|\r| ", ""));
+                            if (ooo > 0)
+                            {
+                                bb.BB3_Zeit[ooo - 1] = Convert.ToInt32(Regex.Replace(x, @"\t|\n|\r| ", ""));
+                            }
+                            ooo++;
                         }
-                        ooo++;
-                    }
-                    break;
-                case "BB4_Zeit":
-                    int oooo = 0;
-                    foreach (string x in splitText[1].Split(new string[] { "Element :" }, StringSplitOptions.None))
-                    {
-                        if (oooo > 0)
+                        break;
+                    case "BB4_Zeit":
+                        int oooo = 0;
+                        foreach (string x in splitText[1].Split(new string[] { "Element :" }, StringSplitOptions.None))
                         {
-                            bb.BB4_Zeit[oooo -1 ] = Convert.ToInt32(Regex.Replace(x, @"\t|\n|\r| ", ""));
+                            if (oooo > 0)
+                            {
+                                bb.BB4_Zeit[oooo - 1] = Convert.ToInt32(Regex.Replace(x, @"\t|\n|\r| ", ""));
+                            }
+                            oooo++;
                         }
-                        oooo++;
-                    }
-                    break;
-            }        
+                        break;
+                }
+            }
+               
+        }
+        else if(levelMode == Level.Menu)
+        {
+
         }
     }
 
